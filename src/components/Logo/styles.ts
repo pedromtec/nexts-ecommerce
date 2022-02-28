592e941b56388fce83ebff1ba58592df7376244b
Pedro Soares <pedromtec@gmail.com>
2022-02-28 13:47:35 -0300

    Hide logo text on mobile

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,10 +1,6 @@
-import { LogoColor, LogoSize } from '.'
+import { LogoProps } from '.'
 import styled, { css } from 'styled-components'
-
-type LogoWrapperProps = {
-  color: LogoColor
-  size: LogoSize
-}
+import media from 'styled-media-query'
 
 const wrapperModifiers = {
   normal: () => css`
@@ -15,12 +11,30 @@ const wrapperModifiers = {
   large: () => css`
     width: 20rem;
     height: 5.9rem;
+  `,
+
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+    
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none
+      }
+    `}
   `
 }
 
-export const Wrapper = styled.div<LogoWrapperProps>`
-  ${({ theme, color, size }) => css`
-    color: ${theme.colors[color]};
-    ${wrapperModifiers[size]}
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color, size, hideOnMobile }) => css`
+    color: ${theme.colors[color!]};
+    ${!!size && wrapperModifiers[size]}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile()}
   `}
 `
